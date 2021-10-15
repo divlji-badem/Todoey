ae4352f768fa2ac61a686980e5a6f829f17facb9
Jelena Tasic <jelenatasicsd@gmail.com>
2021-10-15 23:25:30 +0200

    Creating Item model for data in list, solve the problem for reusable cell.

--- Todoey/Controller/TodoListViewcontroller.swift
+++ Todoey/Controller/TodoListViewcontroller.swift
@@ -9,17 +9,31 @@ import UIKit
 // UITableViewController conforms To UITableViewDataSource, we dont have to write tableView.datasource = self...
 
 class TodoListViewcontroller: UITableViewController {
-
-    var itemArray = ["Find Mike", "Buy Eggos", "Test 3"]
+    
+    var itemArray = [Item]()
+    
     //for local data storage, saves data in plist file
     var defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem1 = Item()
+        newItem1.title = "Test1"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Test2"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Test3"
+        itemArray.append(newItem3)
+        
         //loads data from loacal data
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
+        //        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        //            itemArray = items
+        //        }
     }
     
     //MARK:- Add New Item
@@ -31,7 +45,9 @@ class TodoListViewcontroller: UITableViewController {
             textField = alertTextField
         }
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray,forKey: "ToDoListArray")
             // relod tableView to see added new item
             self.tableView.reloadData()
@@ -45,25 +61,25 @@ class TodoListViewcontroller: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     // Provide a cell object for each row.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       // Fetch a cell of the appropriate type.
-       let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-       
-       // Configure the cell’s contents.
-        cell.textLabel?.text = itemArray[indexPath.row]
-           
-       return cell
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        
+        // Configure the cell’s contents.
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
     }
     //MARK:- TableView Delegate  Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // select and deselect row
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // forces tableview reload data
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,11 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jelena Tasic on 15.10.21..
+//
+
+class Item {
+    var title: String = ""
+    var done: Bool  = false
+}
